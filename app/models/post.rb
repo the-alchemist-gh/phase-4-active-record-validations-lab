0f0dff348092b3ce8c6e6f687e733cf57ccb9f10
the-alchemist-gh <malikbanks01@gmail.com>
2022-10-14 22:34:23 +0000

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    # validate :must_have_some_words
+
+
+    # def must_have_some_words
+    #     unless title.match?(/Won't Believe/)
+    #         errors.add(:title, "Very Wrong")
+    #     end
+    # end
 end
